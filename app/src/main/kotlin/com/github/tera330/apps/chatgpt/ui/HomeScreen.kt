b438787a9f8845e775bc58fe977a5ffc182488f9
tera330 <nonchan877tech@gmail.com>
2024-03-12 14:48:07 +0900

    Drawerのクリックと長押しの切り替え

--- app/src/main/kotlin/com/github/tera330/apps/chatgpt/ui/HomeScreen.kt
+++ app/src/main/kotlin/com/github/tera330/apps/chatgpt/ui/HomeScreen.kt
@@ -1,21 +1,35 @@
 package com.github.tera330.apps.chatgpt.ui
 
+import android.util.Log
+import android.view.MotionEvent
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.combinedClickable
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.DropdownMenuItem
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
 import androidx.compose.material3.DrawerState
 import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -23,15 +37,24 @@ import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.input.pointer.pointerInteropFilter
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontSynthesis.Companion.All
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -48,6 +71,7 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 
 
+@ExperimentalFoundationApi
 @ExperimentalMaterial3Api
 @Composable
 fun HomeScreen(
@@ -171,6 +195,7 @@ fun HomeScreen(
 
 // Drawerに表示するコンテンツ
 @ExperimentalMaterial3Api
+@ExperimentalFoundationApi
 @Composable
 fun DrawerContent(
     conversation: List<Conversation>,
@@ -180,24 +205,74 @@ fun DrawerContent(
     scope: CoroutineScope,
     allClear: (MutableList<Conversation>) -> Unit,
     ) {
+
+    var expanded by remember { mutableStateOf(false) }
+
     Column(modifier = Modifier.padding(16.dp)) {
         Text(text = "ChatGPT", fontSize = 35.sp, modifier = Modifier.padding(bottom = 10.dp))
         Divider()
         // リストアイテムを表示する
         LazyColumn(modifier = modifier.weight(1f)) {
             items(conversation) { item ->
-                Text(
-                    text = "#" + item.title,
-                    fontSize = 30.sp,
+                Box(
                     modifier = Modifier
-                        .padding(5.dp)
+                        .fillMaxWidth()
+                        .combinedClickable(
+                            onClick = {
+                                onItemCLicked(item)
+                                scope.launch {
+                                    drawerState.close()
+                                }
+                            },
+                            onLongClick = {
+                                /*
+                                TwoOptionDropdown(
+                                    options = ,
+                                    selectedOption = ,
+                                    onOptionSelected = ,
+                                    show = 
+                                )
+
+                                 */
+                            }
+                        ),
+                ) {
+                    Text(
+                        text = "#" + item.title,
+                        fontSize = 30.sp,
+                        modifier = Modifier
+                            .padding(5.dp)
+                    )
+                }
+                        /*
+                        .pointerInput(Unit) {
+                            detectTapGestures(
+                                onPress = {
+                                    onItemCLicked(item)
+                                    scope.launch {
+                                        drawerState.close()
+                                    }
+                                },
+                                onLongPress = {
+                                    show = true
+                                }
+                            )
+                            true
+
+
+                         */
+
+                            /*
                         .clickable {
                             onItemCLicked(item)
                             scope.launch {
                                 drawerState.close()
                             }
                         }
-                )
+
+                             */
+
+
                 Divider() // リストアイテムの間に区切り線を追加
             }
         }
@@ -225,3 +300,44 @@ fun DrawerContent(
         }
     }
 }
+
+
+@Composable
+fun TwoOptionDropdown(
+    options: List<String>,
+    selectedOption: String,
+    onOptionSelected: (String) -> Unit,
+    modifier: Modifier = Modifier,
+    show: Boolean
+) {
+    if (show == true) {
+        var expanded by remember { mutableStateOf(false) }
+
+        Box(modifier = modifier) {
+            TextButton(onClick = { expanded = true }) {
+                Text(text = selectedOption)
+                Icon(
+                    imageVector = Icons.Default.ArrowDropDown,
+                    contentDescription = "Dropdown Arrow"
+                )
+            }
+
+            DropdownMenu(
+                expanded = expanded,
+                onDismissRequest = { expanded = false }
+            ) {
+                options.forEach { option ->
+                    DropdownMenuItem(onClick = {
+                        onOptionSelected(option)
+                        expanded = false
+                    }) {
+                        Text(text = option)
+                    }
+                }
+            }
+        }
+    }
+}
+
+ 
+
